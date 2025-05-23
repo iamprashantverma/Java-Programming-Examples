e1ec1c06919cef2ac4bc4a9ba7ac78762dbb55ab
iamprashantverma <prashant2k26@gmail.com>
2025-05-23 14:48:50 +0530

    Order Status enum added

--- EnumDemo.java
+++ EnumDemo.java
@@ -1,5 +1,32 @@
-class EnumDemo{
+enum OrderStatus {
+    PLACED(100, "Order Placed"),
+    SHIPPED(200, "Order Shipped"),
+    DELIVERED(300, "Order Delivered");
+
+    private final int code;
+    private final String description;
+
+    // Constructor
+    OrderStatus(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    // Getter methods
+    public int getStatusCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
+public class EnumDemo{
     public static void main(String[] args) {
-        
+        OrderStatus os = OrderStatus.DELIVERED ;
+        System.out.println(os.getDescription());
+        System.err.println(os.getStatusCode());
+        System.out.println(os.name());
     }
 }
